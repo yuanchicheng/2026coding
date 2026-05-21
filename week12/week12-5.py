5b6d53303ece1d03930cefd40b1976040b099505
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:40:47 +0800

    Update week12-5.py

--- week12/week12-5.py
+++ week12/week12-5.py
@@ -1,1 +1,33 @@
+#week12-5.py
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        path = defaultdict(list)
+        
+        for (a, b), v in zip(equations, values):  # 用拉鍊綁起來
+            path[a].append((b, v))      # 正向走
+            path[b].append((a, 1/v))    # 倒著走
+        
+        def helper(now, target, v0):
+            if now not in path or target not in path:
+                return -1  # 有異物出現，不要再算了
+            
+            if now == target:
+                return v0
+            
+            visited.add(now)
+            ans = -1
+            
+            for node, v in path[now]:
+                if node not in visited:
+                    ans = max(ans, helper(node, target, v0 * v))
+            
+            return ans
+        
+        ans = []
+        
+        for a, b in queries:
+            visited = set()
+            ans.append(helper(a, b, 1))
+        
+        return ans
 
