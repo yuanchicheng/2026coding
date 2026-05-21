2da11699d2e30c4a7b433e489355d5514d303e0e
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:38:16 +0800

    Update week10-2a.py

--- week10/week10-2a.py
+++ week10/week10-2a.py
@@ -1,1 +1,14 @@
+#week10-2a.py
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def helper(root):
+            if root == None:
+                return 0
+            
+            left = helper(root.left)
+            right = helper(root.right)
+            
+            return max(left, right) + 1
+        
+        return helper(root)
 
