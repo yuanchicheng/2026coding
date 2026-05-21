154d0a381b967d0bc6bc4f937750787add178f2a
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:38:44 +0800

    Update week10-3.py

--- week10/week10-3.py
+++ week10/week10-3.py
@@ -1,1 +1,20 @@
+#week10-3.py
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def helper(root, big):
+            if root == None:
+                return 0
+            
+            ans = 0
+            
+            if root.val >= big:
+                ans += 1
+                big = root.val
+            
+            ans += helper(root.left, big)
+            ans += helper(root.right, big)
+            
+            return ans
+        
+        return helper(root, root.val)
 
