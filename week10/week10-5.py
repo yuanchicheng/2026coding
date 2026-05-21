952a87c267ce61d6b93efdcd360599311dc8449b
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:38:58 +0800

    Update week10-5.py

--- week10/week10-5.py
+++ week10/week10-5.py
@@ -1,1 +1,23 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        counter = Counter()
+        counter[0] = 1
+        
+        def helper(root, total):
+            if root == None:
+                return 0
+            
+            total += root.val
+            
+            ans = counter[total - targetSum]
+            counter[total] += 1
+            
+            ans += helper(root.left, total)
+            ans += helper(root.right, total)
+            
+            counter[total] -= 1
+            
+            return ans
+        
+        return helper(root, 0)
 
