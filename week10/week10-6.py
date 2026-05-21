1d40c57aa180da7337c93aaa3eaea0a7c4dfe2c9
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:39:11 +0800

    Update week10-6.py

--- week10/week10-6.py
+++ week10/week10-6.py
@@ -1,1 +1,19 @@
+#week10-6.py
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        
+        def helper(root):
+            if root == None:
+                return 0, 0   # 左邊最長、右邊最長
+            
+            Lans1, Lans2 = helper(root.left)
+            Rans1, Rans2 = helper(root.right)
+            
+            self.ans = max(self.ans, Lans2 + 1, Rans1 + 1)
+            
+            return Lans2 + 1, Rans1 + 1
+        
+        helper(root)
+        return self.ans - 1
 
