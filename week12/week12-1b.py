0a686d5f621b6e10fd3036f44fb7ece091b34fe0
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-21 16:39:56 +0800

    Update week12-1b.py

--- week12/week12-1b.py
+++ week12/week12-1b.py
@@ -1,1 +1,16 @@
+#week12-1b.py
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        
+        def helper(now):  # 函式呼叫函式的版本，也能進行DFS
+            for k in rooms[now]:
+                if k not in visited:
+                    visited.add(k)
+                    helper(k)
+        
+        visited.add(0)
+        helper(0)
+        
+        return len(rooms) == len(visited)
 
